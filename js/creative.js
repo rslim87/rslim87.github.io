dcf0e9b51c5914a36df00607fae0b7d121f537c6
Rachel <rslim87@gmail.com>
2018-01-14 20:11:15 -0500

    working on contact form

--- js/creative.js
+++ js/creative.js
@@ -72,4 +72,30 @@
     }
   });
 
+
+
 })(jQuery); // End of use strict
+
+  //Contact Form
+$(function() {
+    // Get the form.
+    var form = $('#ajax-contact');
+
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+
+    // Set up an event listener for the contact form.
+    $(form).submit(function(event) {
+    // Stop the browser from submitting the form.
+      event.preventDefault();
+
+      var formData = $(form).serialize();
+    
+      $.ajax({
+        type: 'POST',
+        url: $(form).attr('action'),
+        data: formData
+      })
+    
+    });
+});
